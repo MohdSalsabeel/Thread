556bf10ce55510e2d7a5bc31ddde9bbd72e49434
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-05-17 20:33:01 +0530

    Create mutexfile.cpp

---
+++ mutexfile.cpp
@@ -0,0 +1,20 @@
+int myAmount = 0;
+std::mutex m;
+
+void addMoney() {
+	m.lock();
+	++myAmount;
+	m.unlock();
+}
+
+int main()
+{
+	thread t1(addMoney);
+	thread t2(addMoney);
+
+	t1.join();
+	t2.join();
+
+	cout << myAmount << endl;
+	return 0;
+}
