14aedf547d9b98d9a89ee5274f04aa86fe6d824b
MohdSalsabeel <103497056+MohdSalsabeel@users.noreply.github.com>
2022-06-14 23:42:25 +0530

    Create timemutexuntil.cpp

---
+++ timemutexuntil.cpp
@@ -0,0 +1,35 @@
+#include<mutex>
+#include<thread>
+#include<iostream>
+
+using namespace std;
+
+int myAmount = 0;
+timed_mutex m;
+
+void increment(int i)
+{
+	auto now = chrono::steady_clock::now();
+	if (m.try_lock_until(now+chrono::seconds(1)))
+	{
+		++myAmount;
+		this_thread::sleep_for(chrono::seconds(2));
+		cout << "Thread " << i << " Entered" << endl;
+		m.unlock();
+	}
+	else
+	{
+		cout << "Thread " << i << " could not Entered" << endl;
+	}
+}
+
+int main()
+{
+	thread t1(increment, 1);
+	thread t2(increment, 2);
+
+	t1.join();
+	t2.join();
+
+	cout << myAmount << endl;
+}
